db86ce86b72bd9a805e0090ed25e05a6f688e602
Mario Ponce 230 <mario.ponce.230@beliveo.corp>
2019-08-24 10:13:19 -0500

    Se crea un moulo para Decodificar BCD se termina de comentar Tarea1

---
+++ Tarea 2/dec.sv
@@ -0,0 +1,49 @@
+module(input [3:0] bcd, output reg [7:0] DEC);
+    always@(*)
+        begin
+            case(bcd)
+                        //DEC    ABCDEFGP
+                4'b0000:
+                    begin
+                        DEC = 8'b00000000;
+                    end
+                4'b0001:
+                    begin
+                        DEC = 8'b01100000;
+                    end
+                4'b0010:
+                begin
+                        DEC = 8'b11011010;
+                    end
+                4'b0011:
+                begin
+                        DEC = 8'b11100010;
+                    end
+                4'b0100:
+                begin
+                        DEC = 8'b01100110;
+                    end
+                4'b0101:
+                begin
+                        DEC = 8'b10111110;
+                    end
+                4'b0110:
+                begin
+                        DEC = 8'b00111110;
+                    end
+                4'b0111:
+                begin
+                        DEC = 8'b11100000;
+                    end
+                4'b1000:
+                begin
+                        DEC = 8'b11111110;
+                    end
+                4'b1001:
+                begin
+                        DEC = 8'b11100110;
+                    end            
+                default: DEC = 8'b00000001;
+            endcase
+        end
+endmodule
--- tarea1/sumadores.sv
+++ tarea1/sumadores.sv
@@ -5,3 +5,26 @@ module sumador(i1,i2,o1) //this line describe the module, whith the input and ou
     assign o1 = i1 + i2; //this line assign the add of input i1 and input i2 to output o1
 
 endmodule //this line is for close the module
+
+module sumadorif(i1,i2,o1) //this line describe the module, whith the input and output ports
+    input [1:0] i1, i2; //this line describe the bit size of the input ports and the type.
+    output reg [2:0] o1; //this line describe the bit size of the output ports and the type.
+
+    always@(*) //this line describe the open always block
+        begin // begin of the always block
+           if (i1>2'b10) //this line select the number above 2 in the signal i1
+            begin  // begin of the if block
+                o1 = i1 + i2; //this line assign the add of input i1 and input i2 to output o1    
+            end  // end of the if block
+            else //else case
+                o1 = z; // this line asign z on the o1
+        end // end of the always block
+
+    
+
+endmodule //this line is for close the module
+
+module instancia (input [1:0] i1,i2, output [2:0] o1,o2); //this line describe the module, whith the input and output ports
+    instancia sumador (.i1(i1),.i2(i2),.o1(o1)); //this line instance the module sumador
+    instancia sumadorif (.i1(i1),.i2(i2),.o1(o2));//this line instance the module sumadorif
+endmodule //this line is for close the module
