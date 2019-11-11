61760c3683cfe0b0daad3e5796be8fd785242841
Mario Ponce 230 <mario.ponce.230@beliveo.corp>
2019-11-11 17:25:04 -0600

    Actividad 2

--- register/4bits.sv
+++ register/4bits.sv
@@ -1,10 +1,45 @@
-module register(
-    input wire clk,reset,load,
-    input wire [3:0] data_in,
-
-    output reg fn = 0,
-    output reg tx = 0,
-    output reg [4:0] counter = 0
+module register( //this line is for create a module. 
+    input wire clk,reset,st, //is for a input wire
+    input wire [3:0] data_in,//is for a input wire
+    output reg tx = 0//is for a Outout register
 );
+reg [1:0] counter = 0; // this is the register for the couter. 
+reg [3:0] data_in_register; //this is the register for the data input
+reg done = 0; //this register is only a Flag for Done
+reg load = 1; //this register is only a Flag for Data Load
 
+always @* //in this always block i describe the done, and change the load flag for one
+    begin
+        if (done) begin
+            load = 1;
+        end
+    end
+
+always@(posedge clk,posedge reset)//in this always block i describe the function for the module
+    begin
+        if (reset) begin //this if describe a reset function, send 0 for the next registers
+            tx = 0;
+            counter = 0;
+            load = 0;
+        end
+        else begin
+            if (st) begin //this line describe the st
+              if (!load) begin // if load  == 0 the tx output get a array for the data_in
+                tx = data_in_register[counter];
+                counter = counter + 1;
+                if (counter == 3) begin //when the counter == 3 the flag done are 1
+                    done = 1;
+                end
+                end
+                if (load) begin // if load == 0 the register of data in get a data in wire after the flag load get 0
+                    data_in_register = data_in;
+                    load = 0;
+                end  
+            end
+            else begin // else the flag load get 1 and tx get 0. 
+                load = 1;
+                tx = 0;
+            end
+        end
+    end
 endmodule 
--- register/tb.sv
+++ register/tb.sv
@@ -1,42 +1,48 @@
-module register_tb ();
-    reg clk,reset,load;
-    reg [3:0] data_in;
-    wire fn;
-    wire tx;
+module register_tb (); //this module is for the testbeanch 
+    reg clk,reset,st;   //the signal for CLK, reset and st wire. 
+    reg [3:0] data_in;  //the signal for data in wire
+    wire tx; //the signal is for tx register
 
-    register register_ins(
+    register register_ins( //this is for instance the module whit the testbench 
         .clk(clk),
         .reset(reset),
-        .load(load),
+        .st(st),
         .data_in(data_in),
-        .fn(fn),
         .tx(tx)
     );
-    always
+    always // this always block is for describe a clock 
         begin
-            #5 clk = !clk;
+            #5 clk = !clk; // every 5ms the state of CLK change. 
         end
-        initial begin
+        initial begin //this initian is for get the empty states 
         clk = 0;
         reset = 1;
-        load = 0;
+        st = 0;
         data_in = 0;
         #5
-        reset = 1;
+        reset = 0;
         end
-    initial begin
-       reset = 1'b1;
+    initial begin //this initial is for describe te test.
+    $monitor("clk: %b",clk);
+    $monitor("reset: %b",reset);
+    $monitor("st: %b",st);
+    $monitor("tx: %b",tx);
+    $monitor("data_in: %b",data_in);
+    $display("Inicio de la simulacion");
+       reset = 1'b1; // for the empry i will send a 1 and o on the reset.
        #5
        reset = 1'b0;
-       load = 1'b1;
+       st = 1'b1;
        #5
-        data_in[0] = 0;
+        data_in[0] = 1; //after the reset, i will be send the values for the data in
         data_in[1] = 1;
         data_in[2] = 1;
         data_in[3] = 0;
-       #5
-       load = 1'b0;
+       #100 
+       st = 1'b1; // after load the datas i send 1 for the st signal. 
        #50
-       reset = 1;
+       reset = 1; // finaly send a reset. 
+
+       $display("Final de la simulacion");
     end
 endmodule
